cd43cb651ef9b1659e61363fe02b19322c6afda5
Lokraan <Artinaire@gmail.com>
2019-03-05 00:21:26 -0600

    Create base files for the program template.

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,8 +11,19 @@ export default new Router({
       component: require('@/components/LandingPage').default
     },
     {
-      path: '*',
-      redirect: '/'
+      path: '/settings',
+      name: 'settings',
+      component: require('@/components/Settings').default
+    },
+    {
+      path: '/journal',
+      name: 'journal',
+      component: require('@/components/Journal').default
+    },
+    {
+      path: '/trading-page',
+      name: 'trading-page',
+      component: require('@/components/TradingPage').default
     }
   ]
 })
